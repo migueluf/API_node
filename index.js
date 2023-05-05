549839991815529525a4f5fcfadc4acaa24f88f4
Miguel Faria <miguelcontacto2015@gmail.com>
2023-05-05 11:00:21 -0300

    commit inicial

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express')
+
+const userRoute = require('./routes/userRoute')
+
+const app = express()
+const port = 3000
+
+userRoute(app)
+
+app.get('/', (req, res) => res.send('Ola mundo pelo Express!'))
+
+app.listen(port, () => console.log('API rodando na porta 3000'))
---
+++ routes/userRoute.js
@@ -0,0 +1,31 @@
+
+const fs = require('fs')
+const { join } = require('path')
+
+const filePath = join(__dirname, 'users.json')
+
+const getUsers = () => {
+    const data = fs.existsSync(filePath)
+        ? fs.readFileSync(filePath)
+        : []
+
+    try {
+        return JSON.parse(data)
+    } catch (eror) {
+        return []
+    }
+}
+
+const saveUser = (users) => fs.writeFileSync(filePath, JSON.stringfy(users, null, '\t'))
+const userRoute = (app) => {
+    app.route('/users/:id?')
+        .get((req, res) => {
+
+            const users = getUsers()
+
+            res.send({ users })
+        })
+}
+
+
+module.exports = userRoute
